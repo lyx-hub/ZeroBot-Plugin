1c293fc05824d12414e49720c5f56f71d327f803
lyx-hub <71397214+lyx-hub@users.noreply.github.com>
2023-03-07 08:57:59 +0800

    Update main.go

--- main.go
+++ main.go
@@ -246,8 +246,8 @@ func init() {
 	}
 
 	// 通过代码写死的方式添加主人账号
-	 sus = append(sus, 12345678)
-	// sus = append(sus, 87654321)
+	  sus = append(sus, 1479846944)
+	 sus = append(sus, 3531164603)
 
 	if *runcfg != "" {
 		f, err := os.Open(*runcfg)
